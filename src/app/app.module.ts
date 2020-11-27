2a951bdcba08d72fbff14b0c41d274675246b325
johangel <johangel2807@gmail.com>
2020-11-27 16:28:26 -0300

    adding main page and setting inital routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { ProductModule } from './product/product.module';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        MainPageComponent
     ],
     imports: [
         BrowserModule,
